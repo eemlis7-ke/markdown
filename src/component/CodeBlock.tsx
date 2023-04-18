eee80419ef91ab3959af39f2fcb41a23ee085ac2
thr.hryk <thr.hryk@gmail.com>
2023-04-18 15:00:32 +0900

    fix code-heading style

--- src/component/CodeBlock.tsx
+++ src/component/CodeBlock.tsx
@@ -5,6 +5,8 @@ import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
 import { irBlack as theme } from 'react-syntax-highlighter/dist/esm/styles/hljs';
 import { CopyToClipboard } from 'react-copy-to-clipboard';
 import { FiClipboard, FiCheck } from 'react-icons/fi';
+import { FaPython, FaSass } from "react-icons/fa";
+import { SiJavascript } from "react-icons/Si";
 import './CodeBlock.scss';
 import "./prism-theme.css";
 
@@ -23,12 +25,26 @@ const CodeBlock: React.FC<CodeBlockProps> = ({ language, codeString }) => {
     }, 3500);
   };
 
+  const renderIconByLanguage = () => {
+    if (displayLanguage === "python") {
+      return <FaPython />;
+    } else if (displayLanguage === "sass") {
+      return <FaSass />;
+    } else if (displayLanguage === "javascript") {
+      return <SiJavascript />;
+    } else {
+      return null; // 他の言語の場合、アイコンを表示しないか、デフォルトのアイコンを表示
+    }
+  };
+
   const displayLanguage = language ? language.replace("lang-", "") : ""; // lang- をカット
 
   return (
     <div className="code-block">
         <div className="code-heading">
-            <div className="language-name">{displayLanguage}</div> {/* displayLanguage を使用 */}
+            <div className="language-name">
+              {renderIconByLanguage()} {/* アイコンを表示 */}
+              {displayLanguage}</div> {/* displayLanguage を使用 */}
             <div className="copy-button">
                 <CopyToClipboard text={codeString} onCopy={handleCopy}>
                 <button className={isCopied ? 'is-copied' : ''}>
